54f0b36cc3d5786e9a603409de50bbf677cbcc52
Turhan <turhansel@gmail.com>
2021-04-09 14:43:05 +0300

    VideoCard update

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -39,6 +39,46 @@ function RecommendedVideos() {
           channel="Turhan Sel"
           image="https://i.ytimg.com/vi/n9Y2Eb4BaSg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAMoajoKbwaxBckT95fv4NZJijK0w"
         />
+        <VideoCard
+          title="Become a Front End Developer in 1 Hour | 2020"
+          views="1.7M Views"
+          timestamp="2 days ago"
+          channelImage="./profile.jpg"
+          channel="Turhan Sel"
+          image="https://i.ytimg.com/vi/n9Y2Eb4BaSg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAMoajoKbwaxBckT95fv4NZJijK0w"
+        />
+        <VideoCard
+          title="Become a Front End Developer in 1 Hour | 2020"
+          views="1.7M Views"
+          timestamp="2 days ago"
+          channelImage="./profile.jpg"
+          channel="Turhan Sel"
+          image="https://i.ytimg.com/vi/n9Y2Eb4BaSg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAMoajoKbwaxBckT95fv4NZJijK0w"
+        />
+        <VideoCard
+          title="Become a Front End Developer in 1 Hour | 2020"
+          views="1.7M Views"
+          timestamp="2 days ago"
+          channelImage="./profile.jpg"
+          channel="Turhan Sel"
+          image="https://i.ytimg.com/vi/n9Y2Eb4BaSg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAMoajoKbwaxBckT95fv4NZJijK0w"
+        />
+        <VideoCard
+          title="Become a Front End Developer in 1 Hour | 2020"
+          views="1.7M Views"
+          timestamp="2 days ago"
+          channelImage="./profile.jpg"
+          channel="Turhan Sel"
+          image="https://i.ytimg.com/vi/n9Y2Eb4BaSg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAMoajoKbwaxBckT95fv4NZJijK0w"
+        />
+        <VideoCard
+          title="Become a Front End Developer in 1 Hour | 2020"
+          views="1.7M Views"
+          timestamp="2 days ago"
+          channelImage="./profile.jpg"
+          channel="Turhan Sel"
+          image="https://i.ytimg.com/vi/n9Y2Eb4BaSg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAMoajoKbwaxBckT95fv4NZJijK0w"
+        />
       </div>
     </div>
   );
--- src/VideoCard.js
+++ src/VideoCard.js
@@ -6,14 +6,15 @@ function VideoCard({ image, title, channel, views, timestamp, channelImage }) {
   return (
     <div className="videoCard">
       <img className="videocard-thumb" src={image} alt="" />
-      <div className="videocard-info"></div>
-      <Avatar className="videocard-avatar" alt={channel} src={channelImage} />
-      <div className="video-text">
-        <h4>{title}</h4>
-        <p>{channel}</p>
-        <p>
-          {views} • {timestamp}
-        </p>
+      <div className="videocard-info">
+        <Avatar className="videocard-avatar" alt={channel} src={channelImage} />
+        <div className="videocard-text">
+          <h3>{title}</h3>
+          <p>{channel}</p>
+          <p>
+            {views} • {timestamp}
+          </p>
+        </div>
       </div>
     </div>
   );
